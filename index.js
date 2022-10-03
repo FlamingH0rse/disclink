04bd3c0a0e028ba4cc431c1c9eacd5ddf2f5d7f2
99073459+FlamingH0rse@users.noreply.github.com <99073459+FlamingH0rse@users.noreply.github.com>
2022-10-03 16:51:32 +0530

    fr

---
+++ index.js
@@ -0,0 +1,32 @@
+let {token} = require('./config.json')
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.once('ready', () => {
+    console.log('Bot online')
+})
+
+client.on('messageCreate', message => {
+    if (message.content !== '.get') return
+    
+    function randomString(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+    return result;
+    }
+    
+    let finalStr = ''
+    let n = 0
+    while (n <= 70) {
+        finalStr += `https://discord.gg/${randomString(8)}\n`
+    }
+    message.channel.send(finalStr)
+})
+
+
+
+client.login(token)
